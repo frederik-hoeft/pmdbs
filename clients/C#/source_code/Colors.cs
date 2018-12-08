ed39b779451e755317de7f53ec7301605031d3b2
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2018-12-08 15:59:49 +0100

    Add files via upload

---
+++ clients/C#/source_code/Colors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace pmdbs
+{
+    public struct Colors
+    {
+
+        public static Color Orange
+        {
+            get { return Color.FromArgb(255, 96, 49); }
+        }
+        public static Color Gray
+        {
+            get { return Color.FromArgb(64, 64, 64); }
+        }
+        public static Color DarkGray
+        {
+            get { return Color.FromArgb(33, 33, 33); }
+        }
+        public static Color LightGray
+        {
+            get { return Color.FromArgb(220, 220, 220); }
+        }
+    }
+}
