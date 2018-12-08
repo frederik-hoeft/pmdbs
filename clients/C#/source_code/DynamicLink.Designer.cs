7e21ac2dc01562f1ecfa47d57800691cceeb8464
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2018-12-08 15:59:49 +0100

    Add files via upload

---
+++ clients/C#/source_code/DynamicLink.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace pmdbs
+{
+    partial class DynamicLink
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Link = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Link
+            // 
+            this.Link.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Link.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Link.Location = new System.Drawing.Point(0, 0);
+            this.Link.Name = "Link";
+            this.Link.Size = new System.Drawing.Size(219, 51);
+            this.Link.TabIndex = 0;
+            this.Link.TabStop = true;
+            this.Link.Text = "https://example.com";
+            this.Link.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Link_LinkClicked);
+            this.Link.MouseEnter += new System.EventHandler(this.Link_MouseEnter);
+            this.Link.MouseLeave += new System.EventHandler(this.Link_MouseLeave);
+            // 
+            // DynamicLink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Link);
+            this.Name = "DynamicLink";
+            this.Size = new System.Drawing.Size(219, 51);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel Link;
+    }
+}
---
+++ clients/C#/source_code/DynamicLink.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+
+namespace pmdbs
+{
+    public partial class DynamicLink : UserControl
+    {
+        private Color HoverColor;
+        private Color NormalColor;
+        private Boolean Disabled = true;
+
+        public DynamicLink()
+        {
+            InitializeComponent();
+        }
+
+        public Color LinkColorOnMouseHover
+        {
+            get { return HoverColor; }
+            set { HoverColor = value; }
+        }
+
+        public Color LinkColor
+        {
+            get { return NormalColor; }
+            set { NormalColor = value; Link.ForeColor = value; Link.LinkColor = value; }
+        }
+
+        public Font LinkFont
+        {
+            get { return Link.Font; }
+            set { Link.Font = value; }
+        }
+
+        public String LinkText
+        {
+            get { return Link.Text; }
+            set
+            {
+                try
+                {
+                    Uri url = new Uri(value);
+                    string domain = url.Host;
+                    Link.Text = "https://" + domain;
+                    Disabled = false;
+                }
+                catch
+                {
+                    Link.Text = "INVALID URL";
+                    Disabled = true;
+                }
+            }
+        }
+
+        public Color ActiveLinkColor
+        {
+            get { return Link.ActiveLinkColor; }
+            set { Link.ActiveLinkColor = value; }
+        }
+
+        public Color VisitedLinkColor
+        {
+            get { return Link.VisitedLinkColor; }
+            set { Link.VisitedLinkColor = value; }
+        }
+
+        public Boolean LinkVisited
+        {
+            get { return Link.LinkVisited; }
+            set { Link.LinkVisited = value; }
+        }
+
+        private void Link_MouseEnter(object sender, EventArgs e)
+        {
+            Link.LinkColor = HoverColor;
+        }
+
+        private void Link_MouseLeave(object sender, EventArgs e)
+        {
+            Link.LinkColor = NormalColor;
+        }
+
+        private void Link_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (!Disabled)
+            {
+                System.Diagnostics.Process.Start(Link.Text);
+                Link.LinkVisited = true;
+            }
+        }
+    }
+}
