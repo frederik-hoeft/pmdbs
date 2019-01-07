552324ebd15bc94190b60daf9edede38f3bbb2b6
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-01-07 10:49:16 +0100

    Create TimeConverter.cs

---
+++ clients/C#/source_code/TimeConverter.cs
@@ -0,0 +1,9 @@
+namespace pmdbs{
+  public static DateTime UnixTimeStampToDateTime( double unixTimeStamp )
+  {
+      // Unix timestamp is seconds past epoch
+      System.DateTime dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+      dtDateTime = dtDateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
+      return dtDateTime;
+  }
+}
