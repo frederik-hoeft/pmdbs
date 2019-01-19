a66308de687b3b0271e0640a2dd05a157123bb8d
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-01-19 01:33:05 +0100

    Add files via upload

---
+++ clients/C#/source_code/IconData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pmdbs
+{
+    class IconData
+    {
+        private Image image;
+        private String path;
+        public Image Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+    }
+}
