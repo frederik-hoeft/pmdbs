38db5ae1af8fa0cae946c8fc4266d751a35eb5e6
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-05-21 20:05:15 +0200

    Auto-Backup @2019-05-21-200514

--- clients/C#/source_code/HelperMethods.cs
+++ clients/C#/source_code/HelperMethods.cs
@@ -339,6 +339,12 @@ namespace pmdbs
             }
             if (GlobalVarPool.expectedPacketCount == 0)
             {
+                GlobalVarPool.countSyncPackets = false;
+                AutomatedTaskFramework.Tasks.Clear();
+                AutomatedTaskFramework.Task.Create(SearchCondition.In, "LOGGED_OUT|NOT_LOGGED_IN", NetworkAdapter.MethodProvider.Logout);
+                AutomatedTaskFramework.Task.Create(SearchCondition.Match, null, NetworkAdapter.MethodProvider.Disconnect);
+                AutomatedTaskFramework.Tasks.Execute();
+                HelperMethods.FinishSync();
                 GlobalVarPool.Form1.Invoke((System.Windows.Forms.MethodInvoker)delegate
                 {
                     CustomException.ThrowNew.GenericException("Done. Nothing to do.");
