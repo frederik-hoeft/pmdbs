2ebfe9e8b6c0fc98e427768603a7adf7e2993723
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-06-05 18:31:31 +0200

    Commit 2019-06-05-183129

--- clients/C#/source_code/Form1.cs
+++ clients/C#/source_code/Form1.cs
@@ -1,23 +1,16 @@
 ﻿using pmdbs.Properties;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MetroFramework;
-using System.Drawing.Drawing2D;
 using System.Threading;
 using System.Net;
 using System.IO;
 using System.Drawing.Imaging;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
-using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace pmdbs
