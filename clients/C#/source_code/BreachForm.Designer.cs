7544ac5866206b02c1ebd2618965a93beb5cc546
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-07-13 01:13:11 +0200

    Auto-Backup 2019-07-13-011309

--- clients/C#/source_code/BreachForm.Designer.cs
+++ clients/C#/source_code/BreachForm.Designer.cs
@@ -28,21 +28,331 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BreachForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.windowButtonClose = new LunaForms.WindowButton();
+            this.WindowHeaderLabelLogo = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.metroScrollBar1 = new MetroFramework.Controls.MetroScrollBar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.animatedButton1 = new LunaForms.AnimatedButton();
+            this.animatedButton2 = new LunaForms.AnimatedButton();
+            this.animatedButton3 = new LunaForms.AnimatedButton();
+            this.animatedButton5 = new LunaForms.AnimatedButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.metroScrollBar1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(20, 60);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1160, 920);
+            this.panel1.TabIndex = 0;
+            // 
+            // windowButtonClose
+            // 
+            this.windowButtonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.windowButtonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.windowButtonClose.BackgroundColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.windowButtonClose.BackgroundColorNormal = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.windowButtonClose.ImageHover = ((System.Drawing.Image)(resources.GetObject("windowButtonClose.ImageHover")));
+            this.windowButtonClose.ImageNormal = ((System.Drawing.Image)(resources.GetObject("windowButtonClose.ImageNormal")));
+            this.windowButtonClose.Location = new System.Drawing.Point(1138, 7);
+            this.windowButtonClose.Name = "windowButtonClose";
+            this.windowButtonClose.Size = new System.Drawing.Size(60, 50);
+            this.windowButtonClose.TabIndex = 13;
+            // 
+            // WindowHeaderLabelLogo
+            // 
+            this.WindowHeaderLabelLogo.AutoSize = true;
+            this.WindowHeaderLabelLogo.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.WindowHeaderLabelLogo.ForeColor = System.Drawing.Color.White;
+            this.WindowHeaderLabelLogo.Location = new System.Drawing.Point(13, 11);
+            this.WindowHeaderLabelLogo.Name = "WindowHeaderLabelLogo";
+            this.WindowHeaderLabelLogo.Size = new System.Drawing.Size(288, 45);
+            this.WindowHeaderLabelLogo.TabIndex = 14;
+            this.WindowHeaderLabelLogo.Text = "pmdbs breach info";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1160, 150);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(518, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.label1.Location = new System.Drawing.Point(0, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1160, 90);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Microsoft";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(22, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Breach date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label3.Location = new System.Drawing.Point(620, 317);
+            this.label3.MaximumSize = new System.Drawing.Size(500, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(498, 384);
+            this.label3.TabIndex = 3;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(619, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 41);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Description:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label6.Location = new System.Drawing.Point(23, 315);
+            this.label6.MaximumSize = new System.Drawing.Size(500, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(231, 54);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "2015-03-01";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(22, 496);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(281, 41);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Compromized data:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label7.Location = new System.Drawing.Point(23, 435);
+            this.label7.MaximumSize = new System.Drawing.Size(500, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(199, 54);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "14936670";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label8.Location = new System.Drawing.Point(22, 385);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(262, 41);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Affected accounts:";
+            // 
+            // metroScrollBar1
+            // 
+            this.metroScrollBar1.LargeChange = 10;
+            this.metroScrollBar1.Location = new System.Drawing.Point(264, 542);
+            this.metroScrollBar1.Maximum = 100;
+            this.metroScrollBar1.Minimum = 0;
+            this.metroScrollBar1.MouseWheelBarPartitions = 10;
+            this.metroScrollBar1.Name = "metroScrollBar1";
+            this.metroScrollBar1.Orientation = MetroFramework.Controls.MetroScrollOrientation.Vertical;
+            this.metroScrollBar1.ScrollbarSize = 20;
+            this.metroScrollBar1.Size = new System.Drawing.Size(20, 317);
+            this.metroScrollBar1.TabIndex = 11;
+            this.metroScrollBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroScrollBar1.UseSelectable = true;
+            this.metroScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.metroScrollBar1_Scroll);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.animatedButton1);
+            this.flowLayoutPanel1.Controls.Add(this.animatedButton2);
+            this.flowLayoutPanel1.Controls.Add(this.animatedButton3);
+            this.flowLayoutPanel1.Controls.Add(this.animatedButton5);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(252, 317);
+            this.flowLayoutPanel1.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.flowLayoutPanel1);
+            this.panel3.Location = new System.Drawing.Point(32, 542);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(230, 317);
+            this.panel3.TabIndex = 13;
+            // 
+            // animatedButton1
+            // 
+            this.animatedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.animatedButton1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(96)))), ((int)(((byte)(49)))));
+            this.animatedButton1.Depth = 0;
+            this.animatedButton1.Icon = null;
+            this.animatedButton1.Location = new System.Drawing.Point(4, 6);
+            this.animatedButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.animatedButton1.MouseState = LunaForms.AnimatedButton.MouseStateBase.HOVER;
+            this.animatedButton1.Name = "animatedButton1";
+            this.animatedButton1.Primary = false;
+            this.animatedButton1.Size = new System.Drawing.Size(218, 63);
+            this.animatedButton1.TabIndex = 0;
+            this.animatedButton1.Text = "animatedButton1";
+            this.animatedButton1.UseVisualStyleBackColor = true;
+            // 
+            // animatedButton2
+            // 
+            this.animatedButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.animatedButton2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(96)))), ((int)(((byte)(49)))));
+            this.animatedButton2.Depth = 0;
+            this.animatedButton2.Icon = null;
+            this.animatedButton2.Location = new System.Drawing.Point(4, 81);
+            this.animatedButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.animatedButton2.MouseState = LunaForms.AnimatedButton.MouseStateBase.HOVER;
+            this.animatedButton2.Name = "animatedButton2";
+            this.animatedButton2.Primary = false;
+            this.animatedButton2.Size = new System.Drawing.Size(218, 63);
+            this.animatedButton2.TabIndex = 1;
+            this.animatedButton2.Text = "animatedButton2";
+            this.animatedButton2.UseVisualStyleBackColor = true;
+            // 
+            // animatedButton3
+            // 
+            this.animatedButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.animatedButton3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(96)))), ((int)(((byte)(49)))));
+            this.animatedButton3.Depth = 0;
+            this.animatedButton3.Icon = null;
+            this.animatedButton3.Location = new System.Drawing.Point(4, 156);
+            this.animatedButton3.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.animatedButton3.MouseState = LunaForms.AnimatedButton.MouseStateBase.HOVER;
+            this.animatedButton3.Name = "animatedButton3";
+            this.animatedButton3.Primary = false;
+            this.animatedButton3.Size = new System.Drawing.Size(218, 63);
+            this.animatedButton3.TabIndex = 2;
+            this.animatedButton3.Text = "animatedButton3";
+            this.animatedButton3.UseVisualStyleBackColor = true;
+            // 
+            // animatedButton5
+            // 
+            this.animatedButton5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.animatedButton5.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(96)))), ((int)(((byte)(49)))));
+            this.animatedButton5.Depth = 0;
+            this.animatedButton5.Icon = null;
+            this.animatedButton5.Location = new System.Drawing.Point(4, 231);
+            this.animatedButton5.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.animatedButton5.MouseState = LunaForms.AnimatedButton.MouseStateBase.HOVER;
+            this.animatedButton5.Name = "animatedButton5";
+            this.animatedButton5.Primary = false;
+            this.animatedButton5.Size = new System.Drawing.Size(218, 63);
+            this.animatedButton5.TabIndex = 4;
+            this.animatedButton5.Text = "animatedButton5";
+            this.animatedButton5.UseVisualStyleBackColor = true;
             // 
             // BreachForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 1000);
+            this.Controls.Add(this.WindowHeaderLabelLogo);
+            this.Controls.Add(this.windowButtonClose);
+            this.Controls.Add(this.panel1);
             this.Name = "BreachForm";
             this.Style = MetroFramework.MetroColorStyle.Black;
-            this.Text = "BreachForm";
             this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private LunaForms.WindowButton windowButtonClose;
+        private System.Windows.Forms.Label WindowHeaderLabelLogo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private LunaForms.AnimatedButton animatedButton1;
+        private LunaForms.AnimatedButton animatedButton2;
+        private LunaForms.AnimatedButton animatedButton3;
+        private LunaForms.AnimatedButton animatedButton5;
+        private MetroFramework.Controls.MetroScrollBar metroScrollBar1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- clients/C#/source_code/BreachForm.cs
+++ clients/C#/source_code/BreachForm.cs
@@ -15,6 +15,19 @@ namespace pmdbs
         public BreachForm()
         {
             InitializeComponent();
+            Point pt = new Point(this.flowLayoutPanel1.AutoScrollPosition.X, this.flowLayoutPanel1.AutoScrollPosition.Y);
+            this.metroScrollBar1.Minimum = 0;
+            this.metroScrollBar1.Maximum = this.flowLayoutPanel1.DisplayRectangle.Height;
+            this.metroScrollBar1.LargeChange = metroScrollBar1.Maximum / metroScrollBar1.Height + this.flowLayoutPanel1.Height;
+            this.metroScrollBar1.SmallChange = 15;
+            this.metroScrollBar1.Value = Math.Abs(this.flowLayoutPanel1.AutoScrollPosition.Y);
+        }
+
+        private void metroScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            flowLayoutPanel1.AutoScrollPosition = new Point(0, metroScrollBar1.Value);
+            // metroScrollBar1.Invalidate();
+            Application.DoEvents();
         }
     }
 }
