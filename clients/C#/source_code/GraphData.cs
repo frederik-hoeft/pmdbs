c1617ac41618c69b2bc1d88ca6e7788e345af3e3
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-10-18 22:22:54 +0200

    Auto-Backup 2019-10-18-222251

---
+++ clients/C#/source_code/GraphData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pmdbs
+{
+    public class GraphData
+    {
+        private protected int _x = 0;
+        private protected int _y = 1;
+        private protected string _name = "GraphData";
+        private protected Color _color = Color.Navy;
+        public GraphData()
+        {
+
+        }
+        public GraphData(int X, string Name)
+        {
+            _x = X;
+            _name = Name;
+        }
+
+        public GraphData(int X, int Y, string Name)
+        {
+            _x = X;
+            _y = Y;
+            _name = Name;
+        }
+
+        public virtual int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public virtual int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public virtual string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public virtual Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public virtual Point ToPoint()
+        {
+            return new Point(_x, _y);
+        }
+    }
+}
---
+++ clients/C#/source_code/GraphDataF.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pmdbs
+{
+    public class GraphDataF : GraphData
+    {
+        private new float _x = 0f;
+        private new float _y = 1f;
+        public GraphDataF(float X, string Name)
+        {
+            _x = X;
+            _name = Name;
+        }
+
+        public GraphDataF(float X, float Y, string Name)
+        {
+            _x = X;
+            _y = Y;
+            _name = Name;
+        }
+
+        public new virtual float X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public new virtual float Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public new virtual PointF ToPoint()
+        {
+            return new PointF(_x, _y);
+        }
+    }
+}
